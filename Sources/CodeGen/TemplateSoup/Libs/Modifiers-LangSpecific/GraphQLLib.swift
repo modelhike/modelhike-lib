88386217c3f25efbf2f941c90e6addbe1e76a7a8
thrivinghari <172593625+thrivinghari@users.noreply.github.com>
2024-09-21 16:52:25 +0530

    added graphql-typename to generate graphql datatypes in the graphql schema

---
+++ Sources/CodeGen/TemplateSoup/Libs/Modifiers-LangSpecific/GraphQLLib.swift
@@ -0,0 +1,53 @@
+//
+// GraphQLLib.swift
+// DiagSoup
+// https://www.github.com/diagsoup/diagsoup
+//
+
+import Foundation
+
+public struct GraphQLLib {
+    public static var functions: [Modifier] {
+        return [
+            typename
+        ]
+    }
+    
+    
+    public static var typename: Modifier {
+        return CreateModifier.withoutParams("graphql-typename") { (value: Any, lineNo: Int) -> String? in
+            
+            guard let wrapped = value as? TypeProperty_Wrap else {
+                return "----ERROR----"
+            }
+            
+            let prop = wrapped.item
+            
+            switch prop.type {
+                case .int : return "Int"
+                case .float, .double : return "Float"
+                case .bool: return "Boolean"
+                case .string, .id: return "String"
+                case .any: return "Object"
+                case .date: return "Date"
+                case .datetime: return "DateTime"
+                case .buffer: return "Byte[]"
+                case .reference(_):
+                    return "Reference"
+                case .multiReference(_):
+                    return "Reference"
+                case .extendedReference(_):
+                    return "ExtendedReference"
+                case .multiExtendedReference(_):
+                    return "ExtendedReference"
+                case .codedValue(_):
+                    return "CodedValue"
+                case let .customType(typeName):
+                    return typeName
+                case .unKnown:
+                    return "UnKnown"
+            }
+        }
+    }
+    
+}
