2589eb92ac35c74703fefcb02f517434af38d349
thrivinghari <172593625+thrivinghari@users.noreply.github.com>
2024-09-21 16:48:48 +0530

    added sample-value modifier

--- Sources/CodeGen/TemplateSoup/Libs/ModifierLibs/MockDataLib.swift
+++ Sources/CodeGen/TemplateSoup/Libs/ModifierLibs/MockDataLib.swift
@@ -11,7 +11,8 @@ public struct MockDataLib {
     public static func functions(sandbox: Sandbox) -> [Modifier] {
         return [
             sampleJson(sandbox: sandbox),
-            sampleQueryString(sandbox: sandbox)
+            sampleQueryString(sandbox: sandbox),
+            sampleValue(sandbox: sandbox)
         ]
     }
     
@@ -28,6 +29,34 @@ public struct MockDataLib {
         }
     }
 
+    private static func sampleValue(sandbox: Sandbox) -> Modifier {
+        return CreateModifier.withoutParams("sample-value") { (value: Any, lineNo: Int) -> String? in
+            
+            guard let wrapped = value as? TypeProperty_Wrap else {
+                return nil
+            }
+            
+            let prop = wrapped.item
+            
+            let prefix = prop.isArray ? " [" : ""
+            let suffix = prop.isArray ? "]" : ""
+            
+            let num = Int.random(in: 0..<100)
+            let mocking = MockData_Generator()
+            
+            switch prop.type {
+                case .int, .double, .float :
+                    return prefix + " \(num)" + suffix
+                case .bool: return prefix + " true" + suffix
+                case .string: return prefix + " \"\(prop.name) \(num)\"" + suffix
+                case .id: return prefix + " \"" + mocking.randomObjectId_MongoDb() + "\"" + suffix
+                case .any: return prefix + " \"" + mocking.randomObjectId_MongoDb() + "\"" + suffix
+                case .date: return prefix + " \"\(Date.now.ISO8601Format())\"" + suffix
+                default : return ""
+            }
+        }
+    }
+        
     private static func sampleQueryString(sandbox: Sandbox) -> Modifier {
         return CreateModifier.withoutParams("sample-query") { (value: Any, lineNo: Int) -> String? in
             
